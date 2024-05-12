d1311702bb1b7b6a3d2a661dfb6e6c11242958f8
churchrat <mihailkrustev08@gmail.com>
2024-05-12 22:59:16 +0300

    comm1

---
+++ RaisingTheSalary/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaisingTheSalary
+{
+    internal class Person
+    {
+        public Person()
+        {
+            
+        }
+        public Person(string name, double salary, string ocupation, string department)
+        {
+            Name = name;
+            Salary = salary;
+            Occupation = ocupation;
+            Department = department;
+        }
+        public Person(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+        private string name;
+        private double salary;
+        private string ocupation;
+        private string department;
+
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Occupation { get; set; }
+        public string Department { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {this.Name}; Salary: {Math.Round(this.Salary,2)}; Occupation: {this.Occupation}; Department: {this.Department}");
+        }
+        public static void IncreaseSalary(List<Person> people, double bonus)
+        {
+            for (int i = 0; i < people.Count; i++)
+            {
+                double bonusReal = (bonus / 100) * people[i].Salary;
+                people[i].Salary += bonusReal;
+            }
+        }
+    }
+}
---
+++ RaisingTheSalary/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RaisingTheSalary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            Console.WriteLine("How many employees?");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Person person = new Person();
+                Console.Write($"Enter name {i + 1}: ");
+                person.Name = Console.ReadLine();
+                Console.Write($"Enter salary {i + 1}: ");
+                person.Salary = double.Parse(Console.ReadLine());
+                Console.Write($"Enter occupation {i + 1}: ");
+                person.Occupation = Console.ReadLine();
+                Console.Write($"Enter department {i + 1}: ");
+                person.Department = Console.ReadLine();
+                people.Add( person );
+            }
+            Console.WriteLine($"What % do you want to increace the salary? ");
+            double bonus = double.Parse(Console.ReadLine());
+            Person.IncreaseSalary(people, bonus);
+            for (int i = 0;i < n;i++)
+            {
+                people[i].Print();
+            }
+        }
+    }
+}
